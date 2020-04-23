79c249b29c5a42abce492682d08eb5b814eebe32
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-23 01:09:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,8 @@ class School
   end
   
   def add_student(name, grade)
-    @add_student = add_student
+    @student_name = name
+    @grade = grade
     @roster[grade] = []
     @roster[grade] << "name"
   end
